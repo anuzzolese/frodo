e2078e998bc3cc05c526bf30c34a9459226668e3
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2021-11-19 16:26:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 install_requires=[
-   'rdflib>=6.0.2,<=6.0.2',
+   'rdflib',
    'nltk',
    'fredclient @ git+https://github.com/anuzzolese/fredclient'
 ]
