6943fc02776b23a2468c33d39c70a4d3208ce5f0
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2021-11-24 22:22:04 +0100

    Added flask templates and static files.

---
+++ frodo/static/js/demo.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+	$("#btn-opts").click(showOpts);
+	$("#btn-examples").click(showExamples);
+	$(".btn-use").click(useExample);
+	$(".btn-read").click(submit)
+});
+
+function showOpts(){
+    hideExamples();
+	$("#options").show();
+	$("#btn-opts").html("Hide options");
+	$("#btn-opts").unbind("click");
+	$("#btn-opts").click(hideOpts);
+}
+
+function hideOpts(){
+	$("#options").hide();
+	$("#btn-opts").html("Show options");
+	$("#btn-opts").unbind("click");
+	$("#btn-opts").click(showOpts);
+}
+
+function showExamples(){
+	hideOpts();
+	$("#examples").show();
+	$("#btn-examples").html("Hide examples");
+	$("#btn-examples").unbind("click");
+	$("#btn-examples").click(hideExamples);
+}
+
+function hideExamples(){
+	$("#examples").hide();
+	$("#btn-examples").html("Show examples");
+	$("#btn-examples").unbind("click");
+	$("#btn-examples").click(showExamples);
+}
+
+function useExample(){
+	var example = $(".example", $(this).parent()).html();
+	$("#text").html(example);
+}
+
+function submit(){
+	data = {
+    	text: $("#text").val()
+    }
+	$.get(".", data)
+		.done(function(data){
+			alert(data);
+		});
+}
