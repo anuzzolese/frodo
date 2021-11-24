88c29c513ffd030b075861f92f044ba364d75a09
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2021-11-24 21:48:52 +0100

    Added Webapp

---
+++ frodo/webapp.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response, render_template, request
+from frodo import Frodo
+
+myapp = Flask(__name__)
+
+FRED_ENDPOINT = ''
+NS = 'https://w3id.org/stlab/ontology/'
+
+@myapp.route("/")
+def index():
+    text = request.args.get("text")
+    if text:
+        frodo = Frodo(NS, FRED_ENDPOINT)
+        ontology = frodo.generate(text)
+        return Response(ontology.serialize(format='text/turtle'),
+            mimetype='text/turtle',
+            headers={"Content-disposition":
+                 "attachment; filename=ontology.ttl"})
+    else:
+        return render_template("index.html")
+	
+myapp.run()
